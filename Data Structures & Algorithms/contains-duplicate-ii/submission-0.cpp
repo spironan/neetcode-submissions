c0850149a7b1c61494bd52aadf835363542f6c95
Chua Teck Lee <spironan@users.noreply.github.com>
2026-06-15 09:43:23 +0800

    Add: contains-duplicate-ii - submission-0

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        // there must be two int within distance k that are the same value.
+        // we can either 
+        // proof there is a single duplicate within dist k OR
+        // proof by counter-proof, that there exist NO duplicates within dist k
+        unordered_set<int> window{};
+        queue<int> q{};
+        for(auto num: nums) {
+            if(window.contains(num)) {
+                return true;    
+            }
+            window.emplace(num);
+            q.emplace(num);
+            if(window.size() > k) {
+                window.erase(q.front());
+                q.pop();
+            }
+        }
+        return false;
+    }
+};
