1670a8db24945231c693ef48d15090a4ab8de69f
Chua Teck Lee <spironan@users.noreply.github.com>
2026-06-19 13:38:54 +0800

    Add: combination-target-sum - submission-8

---
+++ Data Structures & Algorithms/combination-target-sum/submission-8.cpp
@@ -0,0 +1,44 @@
+class Solution {
+    
+    void backtrack(vector<int>& nums, vector<vector<int>>& res, vector<int>& arr, int i, int t) {
+        // return condition
+        if(t == 0) {
+            res.emplace_back(arr);
+            return;
+        }
+        if(i >= nums.size() || nums[i] > t) {
+            return;
+        }
+
+        //we can choose to pick it
+        // we can further decide how many times to pick it
+        // the criteria is the subtracted target must continue to be larger than current
+        // or choose to skip it
+        
+        // we skip it.
+        backtrack(nums, res, arr, i + 1, t);
+        
+        int count = 0;
+        int num = nums[i];
+        while(t >= num) {
+            t -= num;
+            count++;
+            arr.emplace_back(num);
+            // we pick it
+            backtrack(nums, res, arr, i + 1, t);
+        }
+
+        // we remove
+        while(count--) {
+            arr.pop_back();
+        }
+    }
+public:
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> res;
+        vector<int> arr;
+        backtrack(nums, res, arr, 0, target);   // pass both by reference
+        return res;                              // NRVO: constructed in place, no copy/move
+    }
+};
