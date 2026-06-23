630fd484f18c69baab5b960866ef908b203794fd
Chua Teck Lee <spironan@users.noreply.github.com>
2026-06-23 10:26:39 +0800

    Add: verifying-an-alien-dictionary - submission-3

---
+++ Data Structures & Algorithms/verifying-an-alien-dictionary/submission-3.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isAlienSorted(vector<string>& words, string order) {
+        std::unordered_map<char, int> alienbatical{};
+        for(int i = 0; i < 26; ++i) {
+            alienbatical[order[i]] = i;
+        }
+
+        for(int i = 1; i < words.size(); ++i) {
+            string prev = words[i - 1];
+            string curr = words[i];
+            int j = 0;
+            bool definitelyBigger = false;
+            for(j = 0; j < min(curr.size(), prev.size()); ++j) {
+                if(alienbatical[curr[j]] > alienbatical[prev[j]]){
+                    definitelyBigger = true;
+                    break; // one is definitely bigger than the next, no need to check any further
+                }
+                
+                if(alienbatical[curr[j]] < alienbatical[prev[j]]){
+                    return false;
+                }
+            }
+
+            if(!definitelyBigger && curr.size() < prev.size() && curr[j - 1] == prev[j - 1])
+                return false;
+        }
+
+        return true;
+    }
+};
