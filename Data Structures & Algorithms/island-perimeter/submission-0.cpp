ca099633ffbd25dadd1619fd5fcf7ba150938dd2
Chua Teck Lee <spironan@users.noreply.github.com>
2026-06-23 09:13:10 +0800

    Add: island-perimeter - submission-0

---
+++ Data Structures & Algorithms/island-perimeter/submission-0.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int perimeter = 0;
+        int count = 0;
+        int n = grid.size();
+        int m = grid.front().size();
+
+        for(int i = 0; i < n; ++i) {
+            grid[i].insert(grid[i].begin(), 0);
+            grid[i].emplace_back(0);
+        }
+        grid.insert(grid.begin(), vector(m + 2, 0));
+        grid.emplace_back(vector(m + 2, 0));
+
+        for(int i = 0; i < n; ++i) {
+            for(int j = 0; j < m; ++j) {
+                perimeter += grid[i + 1][j + 1] * 
+                    (
+                        grid[i + 1][j] + grid[i][j + 1] +
+                        grid[i + 2][j + 1] + grid[i + 1][j + 2]
+                    );
+                count += grid[i + 1][j + 1];
+            }
+        }
+        return count * 4 - perimeter;
+    }
+};
